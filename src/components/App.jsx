09a2abcad5b497058c0d905268a89aac0debd8d1
OneTooMany <santosismael.115@gmail.com>
2023-10-14 15:31:04 -0700

    Refactor App component
    Newly added class 'App'
    - Container children components with 'App' class.
    
    Include rendering for Scoreboard

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import "../styles/App.css";
 import Cards from "./Cards";
+import Scoreboard from "./Scoreboard";
 
 function App() {
-  return <Cards />;
+  return (
+    <>
+      <div className="app">
+        <Scoreboard />
+        <Cards />
+      </div>
+    </>
+  );
 }
 
 export default App;
