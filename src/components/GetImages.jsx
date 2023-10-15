7f0d45314a7412cda8e2a628023725132a8ea80f
OneTooMany <santosismael.115@gmail.com>
2023-10-15 14:26:51 -0700

    Refactor Getimages functionality
    Create utitlity function of handleImages
    - Functionality of fetching images is preserved in getImages function.
    - Will only fetch first 18 items from API.
    
    Newly added utility function shuffle
    - Shuffles items using the Fisher-Yates algorithm.

--- src/components/GetImages.jsx
+++ src/components/GetImages.jsx
@@ -1,26 +1,36 @@
 import { useState, useEffect } from "react";
 
-const GetImages = () => {
-  const [gamesArray, setGamesArray] = useState([]);
+export default function handleImages() {
+  const getImages = () => {
+    const [gamesArray, setGamesArray] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      "https://api.rawg.io/api/games?key=e18482c2cd1c4b1685f79cebe248c334",
-      { mode: "cors" },
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setGamesArray(data.results);
-        console.log(gamesArray);
-      })
-      .catch((error) => {
-        console.error("Something went wrong..", error);
-      });
-  }, []);
+    useEffect(() => {
+      fetch(
+        "https://api.rawg.io/api/games?key=e18482c2cd1c4b1685f79cebe248c334",
+        { mode: "cors" },
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setGamesArray(data.results.slice(0, 18));
+          console.log(gamesArray);
+        })
+        .catch((error) => {
+          console.error("Something went wrong..", error);
+        });
+    }, []);
 
-  return gamesArray;
-};
+    return gamesArray;
+  };
 
-export default GetImages;
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  return { getImages, shuffle };
+}
